9bcba71a8681b80f1c3e96cd1e4e63849cda75cc
itwasMark <dequidtm@cardiff.ac.uk>
2022-10-27 14:51:25 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,11 +6,25 @@ Created on Thu Oct 27 13:07:59 2022
 """
 import numpy as np 
 import matplotlib as plt 
+#defining boundary conditions effects on E, the Slap is positioned -a to a hence thickness 2a
 
+def Ei(A,k,n,z):#initial (for where z<-a)
+    return (-1**n)*A*np.exp(complex(0,1)*k*z)#A is an arbitary 
+
+def Em(B,k,n,z,ri):#middle (where -a<z<a)
+    return B*(np.exp(complex(0,1)*np.sqrt(ri)*k*z)+((-1)**n)*np.exp(-complex(0,1)*np.sqrt(ri)*k*z))
+
+def Ef(A,k,z):#final where z>a
+    return A*np.exp(complex(0,1)*k*z)
+
+    
+    
 def Kn(a,ri,n): #wave number eigenvalues
-    alpha=(np.sqrt(refracind)+1)/(np.sqrt(refracind)-1)
-    return (1/(2*alpha*np.sqrt(refracind)))*((np.pi*n)-complex(0,1)*np.log(alpha))
+    alpha=(np.sqrt(ri)+1)/(np.sqrt(refracind)-1)
+    return (1/(2*alpha*np.sqrt(ri)))*((np.pi*n)-complex(0,1)*np.log(alpha))
 
-#where a is dielectric slab thickness, ri is the refractive index, and n=0,1,2... etc
+#where a is  half the dielectric slab thickness, ri is the refractive index, and n=0,1,2... etc
+
+
 
 
