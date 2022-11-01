fb053588df4c125b1e1e24d1a1a6bd2e037a9db0
PhilliposK <116799035+PhilliposK@users.noreply.github.com>
2022-11-01 19:12:55 +0000

    Add files via upload
    I've had a go at completing the functions.py file. Here it's named Definition.py, but it's based on Marks functions.py file. I have also tried to plot E_n vs z for -3a to 3a. Have a look.

---
+++ En_test.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+from Definitions import An, Bn, Kn, Ei, Em, Ef
+
+###############################################################
+#### 1d slab E_n solutions for z<-a, -a<z<a and z>a
+
+
+### Initialisation
+
+
+epsilon_s = 1.8
+a = 10**(-9)
+n = 1
+
+###
+
+
+zi = np.linspace(-3*a, -a, num= 1000, endpoint = False); # z in range [-3a,-a)
+zm = np.linspace(-a,a, num= 1000, endpoint = False); # z in range [-a,a)
+zf = np.linspace(a,3*a, num= 1000, endpoint = True); # z in range [a,3a]
+
+
+k = Kn(a,epsilon_s,n)
+A = An(a,epsilon_s,k)
+B = Bn(a,epsilon_s,n)
+
+E_i = Ei(A,k,n,zi) # E_n array for z in range [-3a,-a)
+E_m = Em(B,k,n,zm,epsilon_s) # E_n array for z in range [-a,a)
+
+####Problem here
+k_f = Kn(a,1.000001,n)
+A = An(a,1.000001,k)
+E_f = Ef(A,k,zf) 
+########################
+
+
+
+plt.plot(zi, E_i,'o') # Plot E_i for -3a to -a
+plt.plot(zm, E_m) # Plot E_m for -a to a
+#plt.plot(zf, E_f,'o') # Plot E_f for a to 3a
+
